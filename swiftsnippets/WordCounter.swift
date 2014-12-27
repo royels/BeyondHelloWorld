73370f980cd68c18d9c47659f9d9364be53a11b8
royels <docmentz@gmail.com>
2014-12-26 18:43:44 -0800

    test commit

---
+++ swiftsnippets/WordCounter.swift
@@ -0,0 +1,20 @@
+//
+//  WordCounter.swift
+//  swiftsnippets
+//
+//  Created by Rohan Yelsangikar on 12/26/14.
+//  Copyright (c) 2014 Royels. All rights reserved.
+//
+
+import Foundation
+
+
+
+func counter(rawString: String) -> {Dictionary<String, Int>()}
+{
+    var iter = rawString.components
+    
+    
+    
+    
+}
