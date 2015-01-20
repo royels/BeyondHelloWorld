711449cf2caeb51f0c2ce95754f43a84dd0cf58c
royels <docmentz@gmail.com>
2015-01-19 23:55:17 -0800

    moar stuff

--- rubysnippets/binaryToDecimal.rb
+++ rubysnippets/binaryToDecimal.rb
@@ -1,42 +1,50 @@
+class Converter
 
+  def initialize()
+    puts "This worked!"
+  end
 
-class Converter(object):
-
-  def decimalToBinary(decimal):
+  def decimalToBinary(decimal)
     counter = 0
-    while True:
-      if decimal < 2**counter:
+    while True
+      if decimal < 2**counter
         break
+      end
       counter += 1
+    end
     return decimalToBinaryHelper( decimal, counter, "")
   end
 
-  
-  def decimalToBinaryHelper( self, decimal, counter, numstring):
-    if counter == -1:
+
+  def decimalToBinaryHelper(decimal, counter, numstring)
+    if counter == -1
       return numstring
-    else:
-      if decimal >= 2**counter:
+    else
+      if decimal >= 2**counter
         numstring += '1'
         decimal -= 2**counter
         counter -=1
-        return self.decimalToBinaryHelper( decimal , counter, numstring)
-      else:
+        return decimalToBinaryHelper( decimal , counter, numstring)
+      else
         numstring += '0'
         counter -= 1
-        return self.decimalToBinaryHelper( decimal, counter, numstring)
+        return decimalToBinaryHelper( decimal, counter, numstring)
+      end
+    end
+  end
 
-  def binaryToDecimal(self, binary):
-
+  def binaryToDecimal(binary)
     binaryString = str(binary)
     decimal = 0
     index = len(binaryString)
-    for char in binaryString:
-	if(char == '1'):
-		sum += 2**index
-	index -= 1
-
-if __name__ == '__main__':
-  conv = Converter()
-  print conv.decimalToBinary(123)
-  print conv.binaryToDecimal(01111011)
+      for char in binaryString
+  	     if(char == '1')
+  		       sum += 2**index
+         end
+  	     index -= 1
+      end
+  end
+end
+conv = Converter.new()
+puts conv.decimalToBinary(123)
+puts conv.binaryToDecimal(01111011)
