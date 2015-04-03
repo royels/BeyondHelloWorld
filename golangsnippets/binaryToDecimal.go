7a8bc56bfef338080febcc55d9ecfc4719f83648
royels <docmentz@gmail.com>
2015-04-02 23:46:49 -0700

    more golang

--- golangsnippets/binaryToDecimal.go
+++ golangsnippets/binaryToDecimal.go
@@ -1,17 +1,41 @@
 package main
 
-import "fmt"
+import "fmt"; "math"
 
 func main() {
-
-
-
+  fmt.Println(decimalToBinary(12))
 }
 
 
 
-func
+func decimalToBinary(decimal float64) float64 {
+  var counter float64 = 0
+  for true {
+    if decimal < (math.Exp2(counter float64)) {
+      break
+    }
+    counter++
+  }
+  return decimalToBinaryHelper(decimal, counter, "")
+
+}
 
 
-
-
+func decimalToBinaryHelper(value float64, counter float64, concat string) string {
+  if value == -1 {
+    return concat
+  }
+  else {
+    if counter >= math.Exp2(counter float64) {
+      concat += '1'
+      value -= math.Exp2(counter float64)
+      counter--
+      return decimalToBinaryHelper(decimal, counter, concat)
+    }
+    else {
+      concat += '0'
+      counter--
+      return decimalToBinaryHelper(decimal, counter, concat)
+    }
+  }
+}
